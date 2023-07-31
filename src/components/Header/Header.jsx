da4eceafe47d3346cbc49f37e39e3ceba662ae8f
Joaquin Villalba <joaquinvillalba8@gmail.com>
2023-07-31 18:13:34 -0300

    hearde con tailwind

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,21 +1,54 @@
-import React from 'react';
-import './Header.css'; // Import the CSS file with the styles
-
+import React from "react";
+import "./Header.css";
+import { UserIcon } from "@heroicons/react/24/outline";
 const Header = () => {
   return (
-    <div className="Header">
-      <div className="MyTinerary">My Tinerary</div>
-      <div className="Nav">
-         {/* cambiar a nav a */}
-        <div className="Home">Home</div>
-        <div className="Cities">Cities</div>
-        <div className="Login">
-            {/* cambiar a button */}
-          <div className="HeroiconsOutlineUser">
-            <div className="Vector"></div>
-          </div>
-          <div className="LoginText">Login</div>
+    <div className="navbar bg-base-100">
+      <div className="navbar-start">
+        <div className="dropdown">
+          <label tabIndex={0} className="btn btn-ghost lg:hidden">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-5 w-5"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M4 6h16M4 12h8m-8 6h16"
+              />
+            </svg>
+          </label>
+          <ul
+            tabIndex={0}
+            className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52"
+          >
+            <li>
+              <a className="Home">Home</a>
+            </li>
+
+            <li>
+              <a className="Cities">Cities</a>
+            </li>
+          </ul>
         </div>
+        <a className="navLogo">My Tinerary</a>
+      </div>
+      <div className="navbar-end hidden lg:flex">
+        <ul className="menu menu-horizontal px-1">
+          <li>
+            <a className="home">Home</a>
+          </li>
+          <li>
+            <a className="cities">Cities</a>
+          </li>
+        </ul>
+        <a className="btn btn-primary login">
+          <UserIcon className="h-5 w-5 flex-none" />Login
+        </a>
       </div>
     </div>
   );
