5d17505dd230fbee271e4154d7b05faddb1695d3
Joaquin Villalba <joaquinvillalba8@gmail.com>
2023-08-02 19:50:35 -0300

    cambio padding

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -10,7 +10,7 @@ const Hero = () => {
         />
         <div>
           <h1 className="text-5xl font-bold">Find the perfect destination</h1>
-          <p className="py-6 text-wrapper">
+          <p className="py-10 text-wrapper">
             Our app will help you find the perfect path for your next trip. With
             an easy-to-use interface and a host of itinerary options, planning
             your next trip has never been easier.
