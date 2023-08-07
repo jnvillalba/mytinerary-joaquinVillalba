97930a1f56627013825810cfdf53099dfe48b99a
Joaquin Villalba <joaquinvillalba8@gmail.com>
2023-08-07 18:20:41 -0300

    padding xl eliminados

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -7,7 +7,7 @@ const Header = ({ textColor }) => {
     color: textColor,
   };
   return (
-    <div className="navbar lg:py-6 lg:px-24 xl:p-32" style={headerStyle}>
+    <div className="navbar lg:py-6 lg:px-24" style={headerStyle}>
       <div className="navbar-start">
         <div className="dropdown">
           <label tabIndex={0} className="btn btn-ghost lg:hidden">
