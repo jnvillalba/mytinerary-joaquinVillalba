b268f2a31e43a523b0e21357c8ac74ca823eccb9
Joaquin Villalba <joaquinvillalba8@gmail.com>
2023-08-09 14:35:43 -0300

    footer con informacion extra

--- src/pages/CitiesPage.jsx
+++ src/pages/CitiesPage.jsx
@@ -1,12 +1,10 @@
 import React from "react";
 import Main from "../layouts/Main";
 import Cities from "../components/Cities/Cities";
-import Header from "../components/Header/Header";
 
 const CitiesPage = () => {
   return (
     <Main background="/cities-bg.jpeg">
-      
       <Cities />
     </Main>
   );
